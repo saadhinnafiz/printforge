1595a854312559b5ecd65d25496b578d38aad21b
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-18 13:09:22 +0500

    feat: add 3D models page with data layer and TypeScript types

---
+++ app/3d-models/page.tsx
@@ -0,0 +1,7 @@
+import { getAllModels } from "@/app/lib/models";
+import type { Model } from "@/app/types";
+
+export default async function ModelsPage() {
+  const models = await getAllModels();
+  return models.map((model: Model) => <p key={model.id}>{model.name}</p>);
+}
---
+++ app/lib/models.ts
@@ -0,0 +1,5 @@
+import modelsData from "../data/models.json";
+
+export async function getAllModels() {
+  return modelsData;
+}
